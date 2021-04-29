e4a64a53462610f198d0a17a2e90b0663e3063ea
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-04-29 16:57:16 -0400

    working on range pramaters like by, max, order

--- server.js
+++ server.js
@@ -1,36 +1,53 @@
 const express = require('express')
 const app = express()
 
-const apps = [
-    {id: 1, name: 'my-app-001'},
-    {id: 2, name: 'my-app-002'},
-    {id: 3, name: 'my-app-003'},
-    {id: 4, name: 'my-app-004'},
-    {id: 5, name: 'my-app-005'},
-    {id: 6, name: 'my-app-006'},
-    {id: 7, name: 'my-app-007'},
-    {id: 8, name: 'my-app-008'},
-    {id: 9, name: 'my-app-009'},
-    {id: 10, name: 'my-app-010'},
-    {id: 11, name: 'my-app-011'},
-    {id: 12, name: 'my-app-012'},
-    {id: 13, name: 'my-app-013'},
-    {id: 14, name: 'my-app-014'},
-    {id: 15, name: 'my-app-015'},
-]
+const apps = []
+
+for (let i = 1; i < 50; i++) {
+    apps.push({
+        id: i,
+        name: `my-app-${i}`
+    })
+}
 
 
 app.get('/apps', (req,res) => {
-    const start = req.query.start - 1
-    const end = req.query.end
-    const max = req.query.max
+    // const start = req.query.start - 1
+    // const end = req.query.end
+    // const max = req.query.max
 
-    const startInd = (start - 1) * end
-    const endInd = start * end
-    
-    const result = apps.slice(start, end)
+    // const startInd = (start - 1) * end
+    // const endInd = start * end
 
     
+    
+    // const result = apps.slice(start, end)
+
+
+    let { by, start, end, max, order } = req.query;
+
+    if(!by) {
+        by = "id"
+    }
+    if(!start) {
+        start = 2
+    }
+    if(!end) {
+        end = 25
+    }
+    if(!max) {
+        max = 30
+    }
+    if(!order) {
+        order = "asc"
+    }
+
+    const limit = parseInt(max)
+    const startIndex = parseInt((start-1)*end)
+    const endIndex = parseInt(start * end)
+
+    const result = apps.slice(startIndex, endIndex)
+
     res.json(result)
 })
 
