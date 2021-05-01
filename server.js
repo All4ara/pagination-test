6c8d822d20af5db320ed23883117e29b344a8a82
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-05-01 17:45:35 -0400

    finished range parameters

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express();
 
-
-app.use(express.json())
+// Seed Data Apps 1 - 50
 const apps = []
-
 for (let i = 1; i < 50; i++) {
     apps.push({
         id: i,
@@ -12,32 +10,27 @@ for (let i = 1; i < 50; i++) {
     })
 }
 
-function compare(a,b) {
-    const nameA = a.name.toLowerCase();
-    const nameB = b.name.toLowerCase();
-
-    let comparison = 0;
-    if(nameA > nameB) {
-        comparison = 1
-    } else if (nameA < nameB) {
-        comparison = -1
-    }
-    return comparison * -1
+// Parse name
+function nameToInt(str) {
+    return parseInt(str.slice(7));
 }
 
+// GET
 app.get('/apps', (req,res) => {
-    let by = req.query.by;
-    let start = req.query.start;
-    let end = req.query.end;
-    let max = req.query.max;
-    let order = req.query.order;
+    const by = req.query.by;
+    const start = req.query.start;
+    const end = req.query.end;
+    const max = req.query.max;
+    const order = req.query.order;
 
     let result = apps
 
+    // When range is passed, "by" is required
     if(!by) {
-        res.status(400).send("beep")
+        res.status(400).send(`When Range Param Passed, "By" must be specified`)
+    // Range => Sort by ID
     } else if (by === "id") {
-        let newResult
+        let newResult = result
         if(start) {
             newResult = result.slice(start - 1)
         }
@@ -47,63 +40,32 @@ app.get('/apps', (req,res) => {
         if(max >= 1) {
             newResult = newResult.slice(0, max)
         }
-        if(order.toLowerCase() === 'desc'){
+        if(order && order.toLowerCase() === 'desc'){
             res.status(200).send(newResult.sort((a,b) => (a.id > b.id) ? -1 : 1))
         } else {
             res.status(200).send(newResult.sort((a,b) => (a.id > b.id) ? 1 : -1))
         }
-
+    // Range => Sort by Name
     } else if(by === "name") {
-         res.status(200).send(result.sort((a,b) => (parseInt(a.name.slice(7)) > parseInt(b.name.slice(7))) ? 1 : -1))
+        let newResult = result
+        if(start) {
+            newResult = result.slice(nameToInt(start) - 1)
+        }
+        if(end && nameToInt(end) >= nameToInt(start) || end && nameToInt(end) >= 0) {
+            nameToInt(end) <=50 ? newResult = result.slice(nameToInt(start) - 1, nameToInt(end)) : newResult = result.slice(nameToInt(start) - 1, 50) 
+        }
+        if(max >= 1) {
+            newResult = newResult.slice(0, max)
+        }
+        if(order && order.toLowerCase() === 'desc'){
+            res.status(200).send(newResult.sort((a,b) => (parseInt(a.name.slice(7)) > parseInt(b.name.slice(7))) ? -1 : 1))
+        } else {
+            res.status(200).send(newResult.sort((a,b) => (parseInt(a.name.slice(7)) > parseInt(b.name.slice(7))) ? 1 : -1))
+        }
     } 
-
-
-    
-
-    
+    // Base Case Default Return
     res.json(result)
 })
 
-
-// app.get('/apps', (req,res) => {
-//     // const start = req.query.start - 1
-//     // const end = req.query.end
-//     // const max = req.query.max
-
-//     // const startInd = (start - 1) * end
-//     // const endInd = start * end
-
-    
-    
-//     // const result = apps.slice(start, end)
-
-
-//     let { by, start, end, max, order } = req.query;
-
-//     if(!by) {
-//         by = "id"
-//     }
-//     if(!start) {
-//         start = 1
-//     }
-//     if(!end) {
-//         end = 25
-//     }
-//     if(!max) {
-//         max = 30
-//     }
-//     if(!order) {
-//         order = "asc"
-//     }
-
-//     const limit = parseInt(max)
-//     const startIndex = parseInt((start-1)*end)
-//     const endIndex = parseInt(start * end)
-
-//     const result = apps.slice(startIndex, endIndex)
-
-//     res.json(result)
-// })
-
-
+// Load Server
 app.listen(3000)
